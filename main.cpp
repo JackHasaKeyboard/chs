b01577577c742a5c9969a9bf5fb1fc91cdc2be7f
jackhasakeyboard <jackhasakeyboard@gmail.com>
2019-08-27 05:51:25 -0600

    Draw board... isn't visible because the perspective is messed up

--- main.cpp
+++ main.cpp
@@ -18,34 +18,34 @@ int main() {
 		100.0f
 	);
 
-	/* Prog board("checker"); */
-	/* board.use(); */
+	Prog board("checker");
+	board.use();
 
-	/* GLuint vao; */
-	/* glGenVertexArrays(1, &vao); */
-	/* glBindVertexArray(vao); */
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 
-	/* const GLfloat vtc[] = { */
-	/* 	-1.0, -1.0, */
-	/* 	1.0, -1.0, */
-	/* 	1.0, 1.0, */
-	/* 	-1.0, 1.0 */
-	/* }; */
+	const GLfloat vtc[] = {
+		-1.0, -1.0,
+		1.0, -1.0,
+		1.0, 1.0,
+		-1.0, 1.0
+	};
 
-	/* GLuint vbo; */
-	/* glGenBuffers(1, &vbo); */
-	/* glBindBuffer(GL_ARRAY_BUFFER, vbo); */
-	/* glBufferData(GL_ARRAY_BUFFER, sizeof vtc, vtc, GL_STATIC_DRAW); */
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof vtc, vtc, GL_STATIC_DRAW);
 
-	/* glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (GLvoid*) 0); */
-	/* glEnableVertexAttribArray(0); */
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (GLvoid*) 0);
+	glEnableVertexAttribArray(0);
 
-	/* // MVP */
-	/* Trans trans; */
-	/* trans.getRot()->x = 90; */
+	// MVP
+	Trans trans;
+	trans.getRot()->x = 90;
 
-	/* glm::mat4 mvp = trans.getMvp(cam); */
-	/* glUniformMatrix4fv(glGetUniformLocation(board.id, "matr"), 1, GL_FALSE, glm::value_ptr(mvp)); */
+	glm::mat4 mvp = trans.getMvp(cam);
+	glUniformMatrix4fv(glGetUniformLocation(board.id, "matr"), 1, GL_FALSE, glm::value_ptr(mvp));
 
 	std::vector<Obj> coll;
 	for (
@@ -106,7 +106,8 @@ int main() {
 
 			disp.clear(0, 0, 0, 1);
 
-			/* glDrawArrays(GL_QUADS, 0, 4); */
+			glBindVertexArray(vao);
+			glDrawArrays(GL_QUADS, 0, 4);
 			for (Obj& piece : coll) {
 				piece.draw(cam);
 			}
