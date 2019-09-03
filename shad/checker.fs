143677baa9dafc1b9daf52df9a1a18fce3893e8f
jackhasakeyboard <jackhasakeyboard@gmail.com>
2019-09-03 15:23:24 -0600

    Highlight bits on board with potential moves, refactor

--- shad/checker.fs
+++ shad/checker.fs
@@ -17,27 +17,30 @@ vec3 purple[2] = vec3[2](
 );
 
 void main() {
-	if ((mod(coord.x, 4) < 2 && mod(coord.y, 4) < 2) || (mod(coord.x, 4) > 2 && mod(coord.y, 4) > 2)) {
-		col = (vec4(grey[0], 1.0));
-	} else {
-		col = vec4(grey[1], 1.0);
+	bool b = (mod(coord.x, 4) < 2 && mod(coord.y, 4) < 2) || (mod(coord.x, 4) > 2 && mod(coord.y, 4) > 2);
+
+	col = (vec4(grey[int(b)], 1.0));
+
+	if ((coord.x < -8) ||(coord.y < -8) ||(coord.x > 8) ||(coord.y > 8)) {
+		col = vec4(grey[0], 1.0);
 	}
 
-	if (
-		coord.x > ((curs.x - 4) * 2) &&
-		coord.x < (((curs.x - 4) * 2) + (1 * 2)) &&
-		coord.y > (((curs.y - 4) * 2)) &&
-		coord.y < (((curs.y - 4) * 2) + (1 * 2))
-	) {
+	if (coord.x > ((curs.x - 4) * 2) &&coord.x < (((curs.x - 4) * 2) + (1 * 2)) &&coord.y > (((curs.y - 4) * 2)) &&coord.y < (((curs.y - 4) * 2) + (1 * 2))) {
 		col = vec4(purple[t], 1.0);
 	}
 
-	if (
-		(coord.x < -8) ||
-		(coord.y < -8) ||
-		(coord.x > 8) ||
-		(coord.y > 8)
+	vec2 move[3] = vec2[3](
+		vec2(2, 3),
+		vec2(2, 1),
+		vec2(3, 1)
+	);
+	for (
+		int i = 0;
+		i < 3;
+		i++
 	) {
-		col = vec4(grey[0], 1.0);
+		if (coord.x > (move[i][0] * 2) &&coord.y > (move[i][1] * 2) &&coord.x < (move[i][0] * 2) + 2 &&coord.y < (move[i][1] * 2) + 2) {
+			col = vec4(mix(grey[int(b)], purple[t], 0.5), 1.0);
+		}
 	}
 }
